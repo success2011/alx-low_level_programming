766ba03a457fff466c39d3da6eab2f25c2f20eac
success2011 <130207007@live.unilag.edu.ng>
2023-06-15 15:20:55 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - A C program that prints exactly Programming is like building a multilingual puzzle
- * Return: 0 (Passed)
+ * Return: 0 (success)
  */
 int main(void)
-puts("\"Programming is like building a multilingual puzzle"");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
