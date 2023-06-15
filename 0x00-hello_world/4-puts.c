c26c20e52dbe6364592a640cddc35cc5b0ed9c0e
success2011 <130207007@live.unilag.edu.ng>
2023-06-15 15:01:51 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints exactly Programming is like building a multilingual puzzle
+ * Return: 0 (Passed)
+ */
+int main(void)
+puts("\"Programming is like building a multilingual puzzle"");
+return (0);
+}
