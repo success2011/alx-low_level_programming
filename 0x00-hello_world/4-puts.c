333aed15efb2b87099466e79b2c25556fa4378cb
success2011 <130207007@live.unilag.edu.ng>
2023-06-15 15:46:43 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A C program that prints exactly Programming is like building a multilingual puzzle
- * Return: 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
