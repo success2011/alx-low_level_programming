42119012a4cc2d381575d41b4c4579b1c6bbbbac
success2011 <130207007@live.unilag.edu.ng>
2023-06-15 19:15:36 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a C program that prints exactly and that piece of art is useful
+ * main - "Empty"
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
