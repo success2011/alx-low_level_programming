bae39850bd9e2f38695c52da167b9be993bf708a
success2011 <130207007@live.unilag.edu.ng>
2023-06-16 19:10:11 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints numbers between 0 to 9 and letters between a to f.
+* Return: 0 (Passed)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; y < 58; m++)
+		putchar(m);
+	for (m = 97; y < 103; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
