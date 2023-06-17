8d286f2192d84010f457ad96318325218d578242
success2011 <130207007@live.unilag.edu.ng>
2023-06-16 18:40:19 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints numbers from 0 to 9.
+* Return: 0 (Passed)
+*/
+int main(void)
+{
+	int y;
+	
+	for (y = 0; y < 10; y++)
+		printf("%d", y);
+	putchar('\n');
+	return (0);
+}
