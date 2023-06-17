952093de24344e8c4b8628de64b48c6b82bc71fc
success2011 <130207007@live.unilag.edu.ng>
2023-06-16 17:55:07 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Alphabet.
+* Return:0 (Passed)
+*/
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	int i;
+	
+	for (y = 0; y < 52; y++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
