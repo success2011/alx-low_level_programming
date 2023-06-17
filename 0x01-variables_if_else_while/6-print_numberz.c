362fe0f9ef693e1382b42019295dca143bbe405b
success2011 <130207007@live.unilag.edu.ng>
2023-06-16 18:51:39 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints numbers from 0 to 9.
+* Return: 0 (Passed)
+*/
+int main(void)
+{
+	int y;
+
+	for (y = 48; y < 58; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+}
