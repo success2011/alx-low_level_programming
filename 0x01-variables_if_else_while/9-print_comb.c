224c95d37e3d6f1674d5422e179dfbf044e0047e
success2011 <130207007@live.unilag.edu.ng>
2023-06-16 19:21:52 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: 0 (Passed)
+*/
+int main(void)
+{
+	int v;
+
+	for (v = 48; n < 58; v++)
+		putchar(v);
+		if (v != 57)
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}
