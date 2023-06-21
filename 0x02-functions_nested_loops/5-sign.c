044b15972c8c2d3b3ec335b7cb61fb0b7f08ab9a
success2011 <130207007@live.unilag.edu.ng>
2023-06-20 18:30:57 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * Print_sign - prints the sign of a numberr
+ * @n: The number to be checked
+ * Return: 1 for positive number, -1 for negative number
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45)
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
