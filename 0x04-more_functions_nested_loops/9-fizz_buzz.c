0ca2827dcf4c4f80018ea21c587273ea4cd9a2e6
success2011 <130207007@live.unilag.edu.ng>
2023-06-23 05:27:27 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	int i;
 
@@ -17,19 +16,19 @@ int main(void)
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
-		} 
+		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
-		} 
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
-		} 
+		}
 		else if (i == 1)
 		{
 			printf("%d", i);
-		} 
+		}
 		else
 		{
 			printf(" %d", i);
