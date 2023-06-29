aca9e9d3839ac872b9f302367d32901cc12b628e
success2011 <130207007@live.unilag.edu.ng>
2023-06-29 08:29:20 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int v;
+	int t;
+
+	for (v = 0; v < n--; v++)
+	{
+		t = a[v];
+		a[v] = a[n];
+		a[n] = t
+	}
+}
+
