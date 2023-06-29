bf418d3a1b6c65b160289518edaf446124bb92c8
success2011 <130207007@live.unilag.edu.ng>
2023-06-29 06:11:33 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,6 +16,10 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = src[j];
 		j++;
 	}
+	 while (j < n)
+        {
+                dest[j] = '\0';
+                j++;
 
 	return (dest);
 }
