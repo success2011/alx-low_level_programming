34e23f017e19540b41c5ae75c9b8d55fdf699c96
sucess2011 <130207007@live.unilag.edu.ng>
2023-07-27 22:14:56 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * Free_list - Linked list
+ * @head: Freed list_t list
  */
 void free_list(list_t *head)
 {
