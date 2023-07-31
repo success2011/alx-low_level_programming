ec88482924ed968fe0b3f22edad62b790fa3805e
sucess2011 <130207007@live.unilag.edu.ng>
2023-07-31 11:21:12 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - A linked list
+ * @head: pointer to the listint_t list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+	return;
+
+	while (*head)
+	{
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	}
+	*head = NULL;
+}
