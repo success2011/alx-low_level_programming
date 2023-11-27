85fc82962cc86eb8672a961a5b22d3095aabe609
success2011 <130207007@live.unilag.edu.ng>
2023-11-27 14:03:39 -0800

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -311,39 +311,3 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	close_elf(o);
 	return (0);
 }
-
-
-2-append_text_to_file.c
-
-#include "main.h"
-
-/**
- * append_text_to_file - This is to append text in file end
- * @filename: This is to Point the name of file created
- * @text_content: This is string to be added
- * Return: -1 in case of fail function, If there is no
- *	write permissions -1, Otherwise - 1.
- */
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int u, nb, dent = 0;
-
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		for (dent = 0; text_content[dent];)
-			dent++;
-	}
-
-	u = open(filename, O_WRONLY | O_APPEND);
-	nb = write(u, text_content, dent);
-
-	if (u == -1 || nb == -1)
-		return (-1);
-
-	close(u);
-
-	return (1);
-}
